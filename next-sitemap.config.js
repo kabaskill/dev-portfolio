f3c3e3a278e3ebff82bf341f886aa98104020936
kabaskill <oguzkabasakal@gmail.com>
2024-11-15 15:06:05 +0100

    sitemap update v4

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,20 +1,10 @@
+
+
 module.exports = {
   siteUrl: "https://oguzkabasakal.com", // Your website URL
   generateRobotsTxt: true, // Generate a robots.txt file
   changefreq: "daily",
   priority: 0.7,
-  exclude: ["/404", "/500"], // Exclude non-essential pages
-  additionalPaths: async (config) => {
-    // This function will dynamically add paths
-    const dynamicPaths = await fetchDynamicPaths();
-    return dynamicPaths.map((path) => ({
-      loc: path, // Full URL of the dynamic page
-      lastmod: new Date().toISOString(),
-    }));
-  },
-};
-
-const fetchDynamicPaths = async () => {
-  const dynamicData = ["/about", "/contact", "/projects", "/experience", "/threescene"];
-  return dynamicData.map((slug) => `https://oguzkabasakal.com${slug}`);
+  exclude: ["/404", "/500", "/admin", "/private"], // Exclude non-essential pages
+  
 };
